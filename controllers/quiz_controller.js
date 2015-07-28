0b800fd48958598b4b914c1d945f86990941f483
ealbutr <alberto.utrilla.del.olmo@ericsson.com>
2015-07-28 12:01:54 +0200

    Validación de entradas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -16,7 +16,7 @@ exports.load = function(req, res, next, quizId) {
 exports.index = function(req, res) {
 	models.Quiz.findAll().then(
 		function(quizes) {
-  		res.render('quizes/index', { quizes: quizes});
+  		res.render('quizes/index', { quizes: quizes, errors: []});
   	}
   ).catch(function(error) {next(error);});
 };  
@@ -24,7 +24,7 @@ exports.index = function(req, res) {
 
 // GET /quizes/:id
 exports.show = function(req, res) {
-  	res.render('quizes/show', { quiz: req.quiz});
+  	res.render('quizes/show', { quiz: req.quiz, errors: []});
 };  
 
 //GET /quizes/:id/answer
@@ -35,7 +35,11 @@ exports.answer = function (req, res) {
 		resultado = 'Correcto';
 	} 
 	res.render('quizes/answer', 
-			{quiz: req.quiz, respuesta: resultado});
+			{quiz: req.quiz, 
+			 respuesta: resultado,
+       errors: []
+      }
+  );
 };
 
 // GET /quizes/new
@@ -44,7 +48,7 @@ exports.new = function(req, res) {
 			{pregunta: "Pregunta:", respuesta: "Respuesta"}
 		);
 		
-		res.render('quizes/new', {quiz: quiz});
+		res.render('quizes/new', {quiz: quiz, errors: []});
 };  
 
 
@@ -52,8 +56,17 @@ exports.new = function(req, res) {
 exports.create = function(req, res) {
 	var quiz = models.Quiz.build(req.body.quiz);
 	
-	//guarda en DB los campos pregunta y respuesta de quiz
-	quiz.save({fields: ["pregunta", "respuesta"]}).then(function() {
-		res.redirect('/quizes');
-	}) //Redirección HTTP (URL relativo) lista de preguntas
+	quiz
+	.validate()
+	.then(
+    function(err){
+      if (err) {
+        res.render('quizes/new', {quiz: quiz, errors: err.errors});
+      } else {
+        quiz // save: guarda en DB campos pregunta y respuesta de quiz
+        .save({fields: ["pregunta", "respuesta"]})
+        .then( function(){ res.redirect('/quizes')}) 
+      }      // res.redirect: Redirección HTTP a lista de preguntas
+    }
+  );
 };
