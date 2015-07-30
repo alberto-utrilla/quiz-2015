f82e985cc6dcaadab43963d7eb47dcee36ff18c6
ealbutr <alberto.utrilla.del.olmo@ericsson.com>
2015-07-30 16:21:48 +0200

    Crear Comentario

---
+++ controllers/comment_controller.js
@@ -0,0 +1,31 @@
+﻿var models = require('../models/models.js');
+
+exports.new = function(req,res){
+	res.render('comments/new', { quizId: req.params.quizId, errors: [] });	
+};
+
+
+exports.create = function(req,res){
+	var comment = models.Comment.build({
+		texto: req.body.comment.texto,
+		QuizId: req.params.quizId
+	});
+	
+	comment
+	.validate()
+	.then(function(error){
+		if(error){
+			res.render('comments/new',{
+				comment: comment, 
+				quizid: req.params.quizId, 
+				errors: error.errors
+			});
+		}else{
+			comment
+			.save()
+			.then(function(){
+				res.redirect('/quizes/' + req.params.quizId)
+				})
+		}
+	}).catch(function(error){next(error)});
+};
